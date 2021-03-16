2ec785078a86f7de0a075b7cf16bf6bf98904448
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-03-16 17:15:11 -0300

    Finish tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,17 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const isAdmin = this.usersRepository.findById(user_id);
+    if (isAdmin.admin === false) {
+      throw new Error("User need admin access");
+    }
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
